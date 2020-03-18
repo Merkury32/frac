8f1088ec192aaeae274761cf7118b9ba5bc4f6e6
Merkury32 <pjozefiak32@gmail.com>
2020-03-18 10:15:32 +0100

    draw() rysuje prostokąt za pomocą putPixel()

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function putPixel(x,y,r,g,b,a) {
-  
+
+  debugger;
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
 
@@ -8,26 +9,40 @@ function putPixel(x,y,r,g,b,a) {
   var id = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
   var pixels = id.data;
 
+  var z = (y * id.width + x) * 4;
+
+  var sr = pixels[z];
+  var sg = pixels[z + 1];
+  var sb = pixels[z + 2];
+
+  console.log(sr,sg,sb);
+
+  var zr = sr*(1-a) + a*r;
+  var zg = sg*(1-a) + a*g;
+  var zb = sb*(1-a) + a*b;
+
+  console.log(zr,zg,zb);
+
+  pixels[z] = Math.round(zr*255);
+  pixels[z + 1] = Math.round(zg*255);
+  pixels[z + 2] = Math.round(zb*255);
+  pixels[z + 3] = Math.round(a*255);
+    
+  ctx.putImageData(id, 0, 0);
+};
+
+function draw(){
   var x = 0;
   var y = 0;
-  var r = 255;
+  var r = 1;
   var g = 0;
   var b = 0;
-  var a = 255;
+  var a = 1;
 
-  debugger;
+  for(i = 0; i <=100; i++){
 
-  for(i = 0; i < canvasHeight; i++){
-    for(j = 0; j < canvasWidth; j++){
-      var z = (y * id.width + x) * 4;
-  
-      pixels[z] = r;
-      pixels[z + 1] = g;
-      pixels[z + 2] = b;
-      pixels[z + 3] = a;
-        
-      ctx.putImageData(id, 0, 0);
-      
+    for(j = 0; j <=100; j++){
+      putPixel(x,y,r,g,b,a);
       x+=1;
     };
     x=0;
