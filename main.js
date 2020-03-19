e321d51b9383abd7b50129605e62fc95aad5463b
Merkury32 <pjozefiak32@gmail.com>
2020-03-19 13:22:22 +0100

    zamieiłem vary na let

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 function putPixel(x,y,r,g,b,a,imageData) {
-  var z = (y * imageData.width + x) * 4;
+  let z = (y * imageData.width + x) * 4;
 
-  var pixels = imageData.data;
+  let pixels = imageData.data;
   
-  var sr = pixels[z];
-  var sg = pixels[z + 1];
-  var sb = pixels[z + 2];
+  let sr = pixels[z];
+  let sg = pixels[z + 1];
+  let sb = pixels[z + 2];
   
-  var zr = sr*(1-a) + a*r;
-  var zg = sg*(1-a) + a*g;
-  var zb = sb*(1-a) + a*b;
+  let zr = sr*(1-a) + a*r;
+  let zg = sg*(1-a) + a*g;
+  let zb = sb*(1-a) + a*b;
 
   pixels[z] = Math.round(zr*255);
   pixels[z + 1] = Math.round(zg*255);
@@ -18,15 +18,15 @@ function putPixel(x,y,r,g,b,a,imageData) {
 };
 
 function draw(){
-  var a = 1;
+  let a = 1;
 
-  var imageData = readPixels();
-  for(i = 0; i <=512; i++){
-    for(j = 0; j <=512; j++){
+  let imageData = readPixels();
+  for(let i = 0; i <=512; i++){
+    for(let j = 0; j <=512; j++){
+      let r = i/512;
+      let g = j/512;
+      let b = j/512;
       putPixel(j,i,r,g,b,a,imageData);
-      var r = i/512;
-      var g = j/512;
-      var b = j/512;
     };
   };
   refreshPixels(imageData);
@@ -34,20 +34,20 @@ function draw(){
 
 function readPixels(){
 
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
+  let canvas = document.getElementById('canvas');
+  let ctx = canvas.getContext('2d');
 
-  var canvasWidth = canvas.width;
-  var canvasHeight = canvas.height;
-  var imageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+  let canvasWidth = canvas.width;
+  let canvasHeight = canvas.height;
+  let imageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
 
   return imageData;
 };
 
 function refreshPixels(imageData){
 
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
+  let canvas = document.getElementById('canvas');
+  let ctx = canvas.getContext('2d');
 
   ctx.putImageData(imageData, 0, 0);
 };
